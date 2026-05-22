371705ecab7be85f3a1ce60d14158887022cea7c
sanjivanSelvarasa <sanjivan_selvarasa@sluz.ch>
2026-05-22 10:31:37 +0200

    Update index.ts add route landingpage

--- Project/Frontend/src/router/index.ts
+++ Project/Frontend/src/router/index.ts
@@ -1,8 +1,15 @@
 import { createRouter, createWebHistory } from 'vue-router'
+import Landingpage from '@/page/Landingpage.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [],
+  routes: [
+    {
+      path: '/',
+      name: 'Landingpage',
+      component: Landingpage,
+    },
+  ],
 })
 
 export default router
