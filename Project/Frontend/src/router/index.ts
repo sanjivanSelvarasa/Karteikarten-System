9c943fe2bc363430a3d9a18179ca8fe3df220fe7
Egor <125749439+Zenderrrr@users.noreply.github.com>
2026-06-05 09:48:20 +0200

    Update index.ts

--- Project/Frontend/src/router/index.ts
+++ Project/Frontend/src/router/index.ts
@@ -1,6 +1,11 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import LandingPage from '@/page/Landingpage.vue'
+<<<<<<< Updated upstream
 import Dashboard from '@/page/Dashboard.vue'
+=======
+import LoginPage from '@/page/Login.vue'
+import RegisterPage from '@/page/Register.vue'
+>>>>>>> Stashed changes
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -11,9 +16,20 @@ const router = createRouter({
       component: LandingPage,
     },
     {
+<<<<<<< Updated upstream
       path: '/dashboard',
       name: 'Dashboard',
       component: Dashboard,
+=======
+      path: "/login",
+      name: "LoginPage",
+      component: LoginPage,
+    },
+    {
+      path: "/register",
+      name: "RegisterPage",
+      component: RegisterPage,
+>>>>>>> Stashed changes
     },
   ],
 })
